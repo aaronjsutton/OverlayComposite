d944b07c80afa8b36c1a5e6a5a9f1836ef126f0d
Aaron Sutton <aaronjsutton@icloud.com>
2017-11-28 09:14:55 -0500

    Fix sample app

--- Sample App/AppDelegate.swift
+++ Sample App/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Sample App
 //
-//  Created by Aaron Sutton on 11/24/17.
+//  Created by Aaron Sutton on 11/28/17.
 //  Copyright © 2017 Aaron Sutton. All rights reserved.
 //
 
@@ -14,15 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
+
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		return true
 	}
 
 	func applicationWillResignActive(_ application: UIApplication) {
+		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
+		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 	}
 
 	func applicationWillEnterForeground(_ application: UIApplication) {
